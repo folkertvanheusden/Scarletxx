f85f123213ae2cdad6e02dfbb0635b54978ab28f
folkert van heusden <mail@vanheusden.com>
2022-04-15 10:38:37 +0200

    uct_node::pick_unvisited

--- uct.cpp
+++ uct.cpp
@@ -4,6 +4,7 @@ uct_node::uct_node(uct_node *const parent, const libataxx::Position *const posit
 	parent(parent),
 	position(position)
 {
+	// TODO: setup 'children'; pre-fill with each move
 }
 
 uct_node::~uct_node()
@@ -27,4 +28,25 @@ void uct_node::add_child(const libataxx::Move & m)
 	new_position->makemove(m);
 
 	children.insert({ m, new uct_node(this, new_position) });
+}
+
+uint64_t uct_node::get_visit_count()
+{
+	return visited;
+}
+
+uint64_t uct_node::get_score()
+{
+	return score;
+}
+
+uct_node *uct_node::pick_unvisited()
+{
+	// check for each child if it had played already
+	for(auto & child : children) {
+		if (child.second->get_visit_count() == 0)
+			return child.second;
+	}
+
+	return nullptr;
 }
--- uct.h
+++ uct.h
@@ -6,13 +6,20 @@
 class uct_node
 {
 private:
-	uct_node                 *const parent;
+	uct_node                 *const parent   { nullptr };
 	std::map<libataxx::Move, uct_node *> children;
-	const libataxx::Position *const position;
+	const libataxx::Position *const position { nullptr };
+	uint64_t                        visited  { 0 };
+	uint64_t                        score    { 0 };
 
 public:
 	uct_node(uct_node *const parent, const libataxx::Position *const position);
 	virtual ~uct_node();
 
-	void add_child(const libataxx::Move & m);
+	void      add_child(const libataxx::Move & m);
+
+	uint64_t  get_visit_count();
+	uint64_t  get_score();
+
+	uct_node *pick_unvisited();
 };
