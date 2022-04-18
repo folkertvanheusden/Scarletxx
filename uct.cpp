d7861c913f3ec58cd9cf73aff750be848151ea42
Folkert van Heusden <mail@vanheusden.com>
2022-04-18 19:34:39 +0200

    properly seed the whole mersenne twister state

--- uct.cpp
+++ uct.cpp
@@ -1,5 +1,6 @@
 // written by folkert van heusden <mail@vanheusden.com>
 // this code is public domain
+#include <algorithm>
 #include <cfloat>
 #include <random>
 #include <mutex>
@@ -7,8 +8,17 @@
 #include "uct.h"
 
 
-std::random_device           rd;
-thread_local std::mt19937_64 gen { rd() };
+auto produce_seed() {
+	std::vector<unsigned int> random_data(std::mt19937::state_size);
+
+	std::random_device source;
+	std::generate(std::begin(random_data), std::end(random_data), [&](){return source();});
+
+	return std::seed_seq(std::begin(random_data), std::end(random_data));
+}
+
+thread_local auto mt_seed = produce_seed();
+thread_local std::mt19937_64 gen { mt_seed };
 
 uct_node::uct_node(uct_node *const parent, const libataxx::Position *const position, const libataxx::Move & causing_move) :
 	parent(parent),
